e7378bac9618c6290afa37ee7895b67264767533
alemait <87567283+alemait@users.noreply.github.com>
2021-11-11 10:35:01 +0300

    Update BrowserHistory.java

--- BrowserHistory.java
+++ BrowserHistory.java
@@ -23,33 +23,13 @@ public class BrowserHistory extends JFrame {
         setSize(600, 400);
         setLocationRelativeTo(null);
         setResizable(false);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        DefaultListModel<String> model = new DefaultListModel<>();
-        JList<String> list = new JList<String>(model);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);       
         for (int i = 0; i < urls.length; i++) {
             JButton button = new JButton();
             button.setText(urls[i]);
-            button.setBounds(10, 20 + i * 30, 200, 25);
-            String s = urls[i];
-            int index = i;
-            button.addActionListener(new ActionListener() {
-                public void actionPerformed(ActionEvent e) {
-                    if (history.contains(s)) return;
-                    if (history.size() >= 5)
-                        history.pollLast();
-                    history.addFirst(s);
-                    model.clear();
-                    Object[] items = history.toArray();
-                    String[] sitems = Arrays.copyOf(items, items.length, String[].class);
-                    model.addAll(Arrays.asList(sitems));
-                    validate();
-                }
-            });
+            button.setBounds(10, 20 + i * 30, 200, 25);            
             add(button);
-        }
-
-        list.setBounds(300, 20, 200, 300);
-        add(list);
+        }        
         setLayout(null);
     }
 
